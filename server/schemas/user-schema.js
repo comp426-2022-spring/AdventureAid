5bf824d70236fffc5169dfdc0e41d84a43d26c07
brennamehl <brennam@email.unc.edu>
2022-04-16 17:18:07 -0400

    added our user model to users/add

--- server/schemas/user-schema.js
+++ server/schemas/user-schema.js
@@ -1,16 +1,16 @@
-const mongoose = require( mongoose );
+const mongoose = require('mongoose');
 const { Schema } = mongoose;
 var userSchema = new mongoose.Schema({
     _id:   {    type:String,
                 lowercase:true},
-    firstname:  String,
+    name:  String,
     vaccinations:{  malaria: {type:Boolean, default:false},
-                    hepatitisA:Boolean,
-                    hepatitisB:Boolean,
-                    yellowFever:Boolean,
-                    tyfoid:Boolean,
-                    dtp:Boolean,
-                    cholera:Boolean
+                    hepatitisA:{type:Boolean, default:false},
+                    hepatitisB:{type:Boolean, default:false},
+                    yellowFever:{type:Boolean, default:false},
+                    tyfoid:{type:Boolean, default:false},
+                    dtp:{type:Boolean, default:false},
+                    cholera:{type:Boolean, default:false}
     },
     languages:[String]
 });

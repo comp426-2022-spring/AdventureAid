ec3d4f30f795f11cce4f676e178a9c9e2d12bc8b
Stephen Buck <stephrb@ad.unc.edu>
2022-04-16 15:26:41 -0400

    mongodb connected

--- server/index.js
+++
@@ -1,5 +0,0 @@
-import { getCountries, createCountriesData } from "./travelbriefing-requests.js"
-import { includeVaccinations, includeLanguages } from "./app-requests.js"
-// Testing fetching all countries json data
-const countries = await getCountries()
-const data = await createCountriesData(countries)
--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
-import express from 'express'
-const PORT = process.env.PORT || 8080;
-
+const express = require("express");
 const app = express();
-
-app.get('/', (req, res) => res.send('Test'));
-
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+// get driver connection
+const dbo = require("./db/conn");
+ 
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
+  console.log(`Server is running on port: ${port}`);
 });

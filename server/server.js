abd12b9a0879f37ed342b344533bb8608c432444
Stephen Buck <stephrb@ad.unc.edu>
2022-04-28 14:03:02 -0400

    typo

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@
         pathCheck ? next() : middleware(req, res, next)
       }
     }
-    app.use('/app/*', unless(verifyJWT, '/app/user/add', '/app/login', '/app/getUsername'))
+    app.use('/app/*', unless(verifyJWT, '/app/users/add', '/app/login', '/app/getUsername'))
   }
   else console.log("Dev options enabled: authentication is not required for endpoint access")
 

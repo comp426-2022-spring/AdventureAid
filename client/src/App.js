a2722292488099f38699487ced018eef8001d059
Stephen Buck <stephrb@ad.unc.edu>
2022-04-29 14:45:50 -0400

    browserRouter

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,19 @@ import './App.css';
 import TestButton from './components/TestButton.jsx';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <TestButton />
-    </div>
+    <BrowserRouter>
+      <Routes>
+      
+        <Route path="" element= {<h1>home</h1>} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+
+      </Routes>
+  </BrowserRouter>
   );
 }
 

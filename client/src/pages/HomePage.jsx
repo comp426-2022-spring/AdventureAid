9de974abba93098ac557e27d7393ff506401690b
Stephen Buck <stephrb@ad.unc.edu>
2022-04-29 19:34:27 -0400

    updated

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,29 +3,96 @@ import './HomePage.css';
 import RequestService from "../services/RequestService";
 
   function HomePage() {
-   const [test, setTest] = useState()
+    const [test, setTest] = useState()
+    
+    // false for all; true for profile
+    const [isProfileCountries, setIsProfileCountries] = useState(false)
 
+    // state for if new data is loading
+    const [isLoading, setIsLoading] = useState(false)
+
+    // recieves and sets state to hold all countries
+    function getAllCountries() {
+      RequestService.getAllCountriesData().then((res) => {
+        setTest(res.data);
+        console.log(res.data);
+      }).catch(error => {
+        console.log(error);
+      })
+    }
+
+    // handles allcountries button press
+    function allCountriesHandler() {
+      setIsProfileCountries(false)
+    }
+
+    // handles profile countries button press
+    function profileCountriesHandler() {
+      setIsProfileCountries(true)
+    }
+
+    // recieves and sets state to hold profile countries
+    function getProfileCountries() {
+      RequestService.getUsername().then(user => {
+        if (user.data.isLoggedIn) {
+          RequestService.getUserCountries(user.data.username).then((res) => {
+            setTest(res.data);
+            console.log(res.data);
+          }).catch(error => {
+            console.log(error);
+          })
+        } else {
+          // if user is not logged in
+          console.log(user.data.message)
+          setTest([{
+              "names":{
+                "name":"not logged in"
+              }
+            }
+          ])
+        }
+      })
+    }
     useEffect(() => {
-    RequestService.getAllCountriesData().then((res) => {
-      setTest(res.data);
-      console.log(res.data);
-    }).catch(error => {
-      console.log(error);
-    })
-})
+      if (!isProfileCountries) {
+        getAllCountries()
+      }
+      else {
+        getProfileCountries()
+      }
+    }, [isProfileCountries, isLoading])
+
+  // if state isn't loaded
+  if (typeof test === 'undefined' || isLoading) {
+    return (
+      <p>loading...</p>
+    )
+  }
+  // if state array is empty
+  if (test.length == 0) {
+    return (
+      <div>
+        <button onClick={allCountriesHandler}>All Countries</button>
+        <button onClick={profileCountriesHandler}>Profile Countries</button>
+        <p>No countries match your profile!</p>
+      </div>
+    )
+  }
   return (
     <pre>
        <div>
+            <button onClick={allCountriesHandler}>All Countries</button>
+            <button onClick={profileCountriesHandler}>Profile Countries</button>
             <p>{
                             test.map(
-                                t => 
-                                <li key = {t.getAllCountriesData.country}>
-                                        <b>{t.getAllCountriesData.country}</b>
-                                </li>
-                            )
+                              t => 
+                              <li key = {t.names.name}>
+                                      <b>{t.names.name}</b>
+                              </li>
+                          )
                         }</p>
         </div>
-    </pre>
+      </pre>
 );
   }
   export default HomePage;

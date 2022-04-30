3206b757c5e084557849b65e643b69fcc1dd0760
Stephen Buck <stephrb@ad.unc.edu>
2022-04-30 19:37:03 -0400

    removed images that don't display correctly

--- client/src/components/CountryButton.jsx
+++ client/src/components/CountryButton.jsx
@@ -1,10 +1,14 @@
 function CountryButton(props) {
     const country = props.country
+
+    function addDefaultSrc(ev) {
+        ev.target.style.visibility = 'hidden'
+    }
     return ( 
         <div>
             <li key = {country} style={{listStyle: "none"}}>
                 <b><button onClick={() => props.clickCountryHandler(country)} className="button-1" style={{marginBottom: 20}}>{country}</button></b>
-                <div> <img width="130" style={{marginBottom: 20}} src={'https://countryflagsapi.com/png/' + country}></img> </div>
+                <div> <img onError={addDefaultSrc} width="130" style={{marginBottom: 20}} src={'https://countryflagsapi.com/png/' + country}></img> </div>
             </li>
         </div>
      );

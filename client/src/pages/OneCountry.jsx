32a2457edbce451f2a6a6e1244c80883dbbe7963
abunts <abunta@me.com>
2022-04-30 15:03:40 -0400

    one country.jsx updated

--- client/src/pages/OneCountry.jsx
+++ client/src/pages/OneCountry.jsx
@@ -1,60 +1,30 @@
 import React, { useEffect, useState }from "react";
 import RequestService from "../services/RequestService";
-function LoginPage() {
+function OneCountry() {
     const [message, setMessage] = useState("")
 
     // Gets username if user is logged in 
-     const getUsername = async () => {
-         await RequestService.getUsername().then((res) => {
+     const getCountryData = async () => {
+         await RequestService.getCountryData(country).then((res) => {
             const data = res.data
-
-            // actions to take if user is logged in
-            if (data.isLoggedIn) {
-                setMessage(data.username)
-                document.querySelector('#username').value = ""
-                document.querySelector('#password').value = ""
-                // redirect to new page?
-            }
-            else 
-                setMessage("Not logged in")
         })
      }
 
     useEffect(() => {
-        getUsername()
+        getCountryData(country);
     }, [])
 
     // handles login button
-    const handleLogin = async () => {
-        const username = document.querySelector('#username').value
-        const password = document.querySelector('#password').value
-        
-        // res contains a json with key "message" that says "success" or "invalid username or password"
-        const res = await RequestService.login(username, password)
-        if (res.data.message != "success") {
-            setMessage(res.data.message)
-        } else {
-            getUsername()
-        }
-        
-    }
-
-    //handles logout button
-    function handleLogout() {
-        // removes the token to un-authenticate user
-        localStorage.removeItem("token")
-        
-        // call to get username to update page if something changes
-        getUsername()
+    const handleCountryClick = async () => {
+        const country = document.querySelector('#country').value;
+        setMessage(true);
     }
 
     return (
         <div>
             <h1>{message}</h1>
-            <input required id="username" type="username" placeholder="username"/>
-            <input required id="password" type="password" placeholder="password"/>
-            <button onClick={handleLogin}>Log In</button> 
-            <button onClick={handleLogout}>Log Out</button>
+            <input required id="onecountry" type="country" placeholder="country"/>
+            <button onClick={handleCountryClick}>Country</button> 
         </div>
     );
 }
